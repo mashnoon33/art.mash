4708b8e8cb906d5249ba0a2033e25814129db6b6
mashny <mashny@mashnys-iMac.local>
2020-05-03 23:57:52 -0500

    Updated files

--- src/pages/q/photos.js
+++ src/pages/q/photos.js
@@ -1,1 +1,13 @@
-export const photos =[{"src":"/images/IMG_9622_mash.studio_q.jpg","width":3,"height":4},{"src":"/images/IMG_9638_mash.studio_q.jpg","width":3,"height":4},{"src":"/images/IMG_9642_mash.studio_q.jpg","width":3,"height":4},{"src":"/images/IMG_9675_mash.studio_q.jpg","width":3,"height":4},{"src":"/images/IMG_9680_mash.studio_q.jpg","width":3,"height":4}]
+export const photos =[{"src":"/images/IMG_9622_mash.studio_q.jpg","width":3,"height":4},{"src":"/images/IMG_9638_mash.studio_q.jpg","width":3,"height":4},
+{ src: '/images/IMG_0172_q.jpg', width: 1333, height: 2000 },
+{ src: '/images/IMG_0175_q.jpg', width: 1333, height: 2000 },
+{ src: '/images/IMG_0189_q.jpg', width: 1333, height: 2000 },
+{ src: '/images/IMG_0191_q.jpg', width: 1333, height: 2000 },
+{ src: '/images/IMG_0195_q.jpg', width: 1333, height: 2000 },
+{ src: '/images/IMG_0199_q.jpg', width: 1333, height: 2000 },
+{ src: '/images/IMG_0256_q.jpg', width: 1333, height: 2000 },
+{ src: '/images/a_q.jpg', width: 1333, height: 2000 },
+{ src: '/images/b_q.jpg', width: 1333, height: 2000 },
+{ src: '/images/c_q.jpg', width: 1333, height: 2000 },
+
+{"src":"/images/IMG_9642_mash.studio_q.jpg","width":3,"height":4}]
